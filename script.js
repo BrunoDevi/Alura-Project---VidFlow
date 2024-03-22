1884355ee22db8eb7fce80614eca0525303c197a
Bruno Baêta <brunoantonio25@outlook.com>
2024-03-21 21:18:37 -0300

    refatorando

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const videosContainer = document.querySelector('.videos__container')
 
-const API = fetch("http://localhost:3000/videos")
-.then(res => res.json())
-.then((videos)=>
-    videos.forEach((video)=>{
-        videosContainer.innerHTML += `
-        <li class="videos__item">
-            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img class="img-canal" src="${video.imagem}" alt="logo do canal">
-                <h3 class="titulo-video">${video.titulo}</h3>
-                <p class="titulo-canal">${video.descricao}</p>
-            </div>
-        </li>
-        `;
-    })
-)
-.catch(error => {
-    videosContainer.innerHTML = `<li>Houve um ERROR ao carregar a Pagina. error -> ${error} </li>`;
-})
+async function exibirVideos(){
+    try{
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
+
+        videos.forEach((video) => {
+            if(video.categoria == ""){
+                throw new Error(`O video ${video.titulo} não possui categoria.`)
+            }
+
+            videosContainer.innerHTML += `
+            <li class="videos__item">
+                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img class="img-canal" src="${video.imagem}" alt="logo do canal">
+                    <h3 class="titulo-video">${video.titulo}</h3>
+                    <p class="titulo-canal">${video.descricao}</p>
+                </div>
+            </li>
+            ` 
+        })
+    }catch(error){
+        videosContainer.innerHTML = `
+            <li>Houve um error durante a exibição dos videos. error -> ${error}</li>
+        `
+    }
+}
+exibirVideos();
